d37711f8f8d624adc9b752448234921c2a09198c
DarkKnight450 <64155209+DarkKnight450@users.noreply.github.com>
2021-11-29 23:08:23 +0530

    Add files via upload

---
+++ docs/auto-mount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# we'll edit this file
+sudo cat /etc/fstab
+# making a directory where I wanna mount it
+sudo mkdir /mnt/Snorlax
+sudo blkid
+sudo nano /etc/fstab
+cat /etc/fstab
+# UUID=C8329D20329D1512	/mnt/Snorlax	ntfs	defaults	0	2
+# this line above is appended to the /etc/fstab file
---
+++ docs/get-grub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install grub-efi grub2-common grub-customizer
+sudo grub-install
+sudo cp /boot/grub/x86_64-efi/grub.efi /boot/efi/EFI/pop/grubx64.efi
